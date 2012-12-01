9d59615580efa6554acc30a9ac245eaf96c192bc
crusierr <crusierr@gmail.com>
2012-12-01 18:28:15 +0000

    aaaaa

--- Alert Tsunami/src/unap/icci/alertsunami/Main.java
+++ Alert Tsunami/src/unap/icci/alertsunami/Main.java
@@ -2,6 +2,6 @@ package unap.icci.alertsunami;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("hola chichi");	
+		System.out.println("hola chichichichic");	
 	}
 }
