8665d67037b30470a39caab8545eab34083f9c37
crgarrido.s@gmail.com <crgarrido.s@gmail.com@4bce4bf9-6550-0d38-5ae3-8cf01e3bfa03>
2012-12-01 20:25:38 +0000

    limpio??

--- trunk/Alert Tsunami/src/unap/icci/alerttsunami/bll/Punto.java
+++ trunk/Alert Tsunami/src/unap/icci/alerttsunami/bll/Punto.java
@@ -2,11 +2,15 @@ package unap.icci.alerttsunami.bll;
 
 public class Punto {
 
-	public static final float radio = 10;
+	public static final float RADIO = 10;
 	public Coordenada coordenada;
 
 	public Punto(){
 
+	}
+	
+	public Punto(Coordenada coordenada){
+		this.setCoordenada(coordenada);
 	}
 
 	public void finalize() throws Throwable {
@@ -14,7 +18,10 @@ public class Punto {
 	}
 
 	public Coordenada getCoordenada(){
-		return null;
+		return coordenada;
 	}
 
+	public void setCoordenada(Coordenada coordenada) {
+		this.coordenada = coordenada;
+	}
 }
