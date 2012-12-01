09afbeda35aed0d6b1822496aaebbbb233aa041c
dcadiz.g <dcadiz.g@gmail.com>
2012-12-01 20:18:07 +0000

    Clase Comentario listo

--- Alert Tsunami/src/unap/icci/alerttsunami/bll/Comentario.java
+++ Alert Tsunami/src/unap/icci/alerttsunami/bll/Comentario.java
@@ -6,22 +6,41 @@ public class Comentario {
 
 	private String contenido;
 	private Date fecha;
-	public Usuario usuario;
+	private Usuario usuario;
 
-	public Comentario(){
-
+	//constructor
+	public Comentario(String contenido, Usuario usuario){
+		setContenido(contenido);
+		this.fecha = new Date();
+		setUsuario(usuario);
+	}
+	
+	//get y set contenido
+	public String getContenido() {
+		return contenido;
 	}
 
+	public void setContenido(String contenido) {
+		this.contenido = contenido;
+	}
+	
+	//get fecha
+	public Date getFecha() {
+		return fecha;
+	}
+	
+	//get y set usuario
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	//destructor
 	public void finalize() throws Throwable {
 
 	}
-
-	/**
-	 * 
-	 * @param contenido
-	 */
-	public Comentario(String contenido){
-
-	}
-
+	
 }
