cff3377f21bbcab2410dbd3a1269f0bf68fdb32b
dcadiz.g@gmail.com <dcadiz.g@gmail.com@4bce4bf9-6550-0d38-5ae3-8cf01e3bfa03>
2012-12-01 20:50:21 +0000

    Clase Dispositivo listo

--- trunk/Alert Tsunami/src/unap/icci/alerttsunami/bll/Dispositivo.java
+++ trunk/Alert Tsunami/src/unap/icci/alerttsunami/bll/Dispositivo.java
@@ -3,59 +3,66 @@ package unap.icci.alerttsunami.bll;
 public class Dispositivo {
 
 	private Boolean estadoDeRiesgo;
-	public Ciudad ciudad;
-	public Usuario usuario;
-	public Coordenada coordenada;
+	private Ciudad ciudad;
+	private Usuario usuario;
+	private Coordenada posicion;
+	private Integer intervalo;
 
-	public Dispositivo(){
-
+	public Dispositivo(Ciudad ciudad, Usuario usuario, Coordenada posicion){
+		this.estadoDeRiesgo = null;
+		setCiudad(ciudad);
+		setUsuario(usuario);
+		setPosicion(posicion);
+	}
+	
+	//get estado de riesgo
+	public Boolean getEstadoDeRiesgo(){
+		return this.estadoDeRiesgo;
+	}
+	
+	//get y set ciudad
+	public Ciudad getCiudad(){
+		return ciudad;
+	}
+	
+	public void setCiudad(Ciudad ciudad){
+		this.ciudad = ciudad;
+	}
+	
+	//get y set usuario
+	public Usuario getUsuario(){
+		return usuario;
+	}
+	
+	public void setUsuario(Usuario usuario){
+		this.usuario = usuario;
+	}
+	
+	//get y set posición
+	public Coordenada getPosicion(){
+		return posicion;
+	}
+	
+	public void setPosicion(Coordenada posicion){
+		this.posicion = posicion;
 	}
 
+	//get y set intervalo en que se demora en actualizar la posición del dispositivo
+	public Integer getIntervalo(){
+		return intervalo;
+	}
+	
+	public void setIntervalo(Integer intervalo){
+		this.intervalo = intervalo;
+	}
+	
+	//destructor
 	public void finalize() throws Throwable {
 
 	}
-
-	/**
-	 * 
-	 * @param c
-	 */
+	
+	//otros
 	public void actualizarPosicion(Coordenada c){
-
-	}
-
-	public Boolean estaEnRiego(){
-		return false;
-	}
-
-	public int getIntervaloActualizacion(){
-		return 0;
-	}
-
-	public Coordenada getPosicion(){
-		return null;
-	}
-
-	/**
-	 * 
-	 * @param enRiesgo
-	 */
-	public void setEstadoRiego(boolean enRiesgo){
-
-	}
-
-	/**
-	 * 
-	 * @param intervalo
-	 */
-	public void setIntervaloActualizacion(int intervalo){
-
-	}
-
-	/**
-	 * 
-	 * @param c
-	 */
-	private void setPosicion(Coordenada c){
 
 	}
 
