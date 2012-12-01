54e284c2553eee73d15556af3f66962abc7f3589
dcadiz.g@gmail.com <dcadiz.g@gmail.com@4bce4bf9-6550-0d38-5ae3-8cf01e3bfa03>
2012-12-01 20:24:06 +0000

    Clase Coordenada lista

--- trunk/Alert Tsunami/src/unap/icci/alerttsunami/bll/Coordenada.java
+++ trunk/Alert Tsunami/src/unap/icci/alerttsunami/bll/Coordenada.java
@@ -5,20 +5,33 @@ public class Coordenada {
 	private float latitud;
 	private float longitud;
 
-	public Coordenada(){
-
+	//constructor
+	public Coordenada(float latitud, float longitud){
+		setLatitud(latitud);
+		setLongitud(longitud);
 	}
 
+	//get y set latitud
+	public Float getLatitud(){
+		return latitud;
+	}
+	
+	public void setLatitud(float latitud){
+		this.latitud = latitud;
+	}
+	
+	//get y set longitud
+	public Float getLongitud(){
+		return longitud;
+	}
+	
+	public void setLongitud(float longitud){
+		this.longitud = longitud;
+	}
+	
+	//destructor
 	public void finalize() throws Throwable {
 
-	}
-
-	public Float getLatitud(){
-		return null;
-	}
-
-	public Float getLongitud(){
-		return null;
-	}
+	}	
 
 }
