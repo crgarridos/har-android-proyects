6ab7c0aa3004ff47874b1d2204701d5c5656d373
crgarrido.s <crgarrido.s@gmail.com>
2012-12-01 18:04:58 +0000

    Creada la capa de negocio (bll: bussiness logic layer)

---
+++ Alert Tsunami/src/unap/icci/alerttsunami/bll/PuntoEncuentro.java
@@ -0,0 +1,18 @@
+public class PuntoEncuentro extends Punto {
+
+	public GrupoFamiliar grupoFamiliar;
+	public Ciudad ciudad;
+
+	public PuntoEncuentro(){
+
+	}
+
+	public void finalize() throws Throwable {
+		super.finalize();
+	}
+
+	public GrupoFamiliar getGrupoFamiliar(){
+		return null;
+	}
+
+}
