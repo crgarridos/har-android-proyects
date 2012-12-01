2772a68b1f183f837ddde2ea5a33cbfffe1d45f2
crgarrido.s <crgarrido.s@gmail.com>
2012-12-01 19:37:28 +0000

    Clase Usuario limpia e implementados getters y setters sin logica de negocio

--- Alert Tsunami/src/unap/icci/alerttsunami/bll/Usuario.java
+++ Alert Tsunami/src/unap/icci/alerttsunami/bll/Usuario.java
@@ -6,33 +6,86 @@ public class Usuario {
 
 	private String email;
 	private Boolean estadoDeLlegada;
-	private String nombre;
+	private String nombreCompleto;
 	private String nombreUsuario;
 	private String password;
-	public ArrayList<Dispositivo> dispositivos;
-	public ArrayList<Comentario> comentarios;
-	public ArrayList<Invitacion> invitaciones;
-	public GrupoFamiliar grupoFamiliar;
-
+	private ArrayList<Dispositivo> dispositivos;
+	private ArrayList<Comentario> comentarios;
+	private ArrayList<Invitacion> invitaciones;
+	private GrupoFamiliar grupoFamiliar;
+	
+	//Constructores
 	public Usuario(){
-
+		dispositivos = new ArrayList<Dispositivo>();
+		comentarios = new ArrayList<>();
+		invitaciones = new ArrayList<>();
 	}
-
+	
 	public void finalize() throws Throwable {
 
 	}
 
-	/**
-	 * 
-	 * @param nombreCompleto
-	 * @param nombreUsuario
-	 * @param pass
-	 * @param email
-	 */
 	public Usuario(String nombreCompleto, String nombreUsuario, String pass, String email){
-
+		this.setNombreCompleto(nombreCompleto);
+		this.setNombreUsuario(nombreUsuario);
+		this.setPassword(pass);
+		this.setEmail(email);
+	}//Fin contructores
+	
+	// Setters y getters
+	public String getNombreUsuario() {
+		return nombreUsuario;
 	}
 
+	public void setNombreUsuario(String nombreUsuario) {
+		this.nombreUsuario = nombreUsuario;
+	}
+
+	public String getNombreCompleto(){
+		return nombreCompleto;
+	}
+
+	public void setNombreCompleto(String nombreCompleto){
+		this.nombreCompleto=nombreCompleto;
+	}
+
+	public String getEmail(){
+		return email;
+	}
+	
+	public void setEmail(String email){
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setEstadoLlegada(boolean llego){
+		this.estadoDeLlegada = llego;
+	}
+	
+	public Boolean isEstadoLlegada(){
+		return estadoDeLlegada;
+	}
+	
+	public Boolean estaLlegada(){
+		return estadoDeLlegada;
+	}
+
+	public ArrayList<Dispositivo> getDispositivos() {
+		return dispositivos;
+	}
+
+	public void addDispositivo(Dispositivo dispositivo) {
+		this.dispositivos.add(dispositivo);
+	}
+	
+	//Fin setters y getters
 	public void abandonarGrupoFamiliar(){
 
 	}
@@ -41,16 +94,12 @@ public class Usuario {
 
 	}
 
-	/**
-	 * 
-	 * @param cadena
-	 */
 	public Usuario buscar(String cadena){
 		return null;
 	}
 
 	public void comentar(){
-
+		
 	}
 
 	protected void definirPuntoEncuentro(){
@@ -61,97 +110,39 @@ public class Usuario {
 
 	}
 
-	public boolean esLider(){
+	public Boolean esLider(){
 		return false;
 	}
 
-	public boolean estaLlegada(){
+	public Boolean existe(String nombreUsuario){
 		return false;
 	}
 
-	/**
-	 * 
-	 * @param nombreUsuario
-	 */
-	public boolean existe(String nombreUsuario){
-		return false;
-	}
-
-	/**
-	 * 
-	 * @param nombre
-	 * @param pass
-	 */
-	public boolean existe(String nombre, String pass){
+	public Boolean existe(String nombre, String pass){
 		return false;
 	}
 
 	public ArrayList<Comentario> getComentarios(){
-		return null;
-	}
-
-	public String getEmail(){
-		return "";
+		return comentarios;
 	}
 
 	public GrupoFamiliar getGrupoFamiliar(){
-		return null;
+		return grupoFamiliar;
 	}
 
 	public ArrayList<Invitacion> getInvitaciones(){
-		return null;
+		return invitaciones;
 	}
 
-	public String getNombreCompleto(){
-		return "";
-	}
-
-	/**
-	 * 
-	 * @param invitacion
-	 */
 	public void onInvitacionRecibida(Invitacion invitacion){
-
+		
 	}
 
 	public void persistir(){
 
 	}
 
-	/**
-	 * 
-	 * @param email
-	 */
-	public void setEmail(String email){
-
-	}
-
-	/**
-	 * 
-	 * @param llego
-	 */
-	public void setEstadoLlegada(boolean llego){
-
-	}
-
-	/**
-	 * 
-	 * @param nombreUsuario
-	 */
-	public void setNombreCompleto(String nombreUsuario){
-
-	}
-
-	/**
-	 * 
-	 * @param email
-	 */
-	public void setPass(String email){
-
-	}
-
 	public void vaciarInvitaciones(){
-
 	}
 
 }
