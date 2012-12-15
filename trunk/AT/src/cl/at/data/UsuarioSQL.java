5dfcc6b6a0044b184bd5c248792d771b1385d8bc
crusierr@gmail.com <crusierr@gmail.com@4bce4bf9-6550-0d38-5ae3-8cf01e3bfa03>
2012-12-15 02:28:39 +0000

    Se agregaron los metodos persistir

--- trunk/AT/src/cl/at/data/UsuarioSQL.java
+++ trunk/AT/src/cl/at/data/UsuarioSQL.java
@@ -1,9 +1,5 @@
 package cl.at.data;
 
-import java.util.ArrayList;
-
-import org.apache.http.NameValuePair;
-import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -14,7 +10,7 @@ import cl.at.util.Parametros;
 public class UsuarioSQL {
 
 	private static final String TAG = UsuarioSQL.class.getName();
-	String URL_connect = "http://acinfo.unap.cl/jvega/Alerttsunami/getPersona.php";
+	String URL_connect = "http://acinfo.unap.cl/jvega/Alerttsunami/";
 	ConexionHttp post;
 
 	public UsuarioSQL() {
@@ -26,16 +22,12 @@ public class UsuarioSQL {
 			Parametros postParametersToSend = new Parametros();
 			postParametersToSend.add("nombre", u.getNombreUsuario());
 			postParametersToSend.add("pass", u.getPassword());
-//			ArrayList<NameValuePair> postParametersToSend = new ArrayList<NameValuePair>();
-//			postParametersToSend.add(new BasicNameValuePair("nombre", u.getNombreUsuario()));
-//			postParametersToSend.add(new BasicNameValuePair("pass", u.getPassword()));
 			JSONArray jdata = null;
-			jdata = post.getServerData(postParametersToSend, URL_connect);
+			jdata = post.getServerData(postParametersToSend, URL_connect + "getPersona.php");
 			if (jdata != null) {
 				try {
 					JSONObject json_data = jdata.getJSONObject(0); // Se lee la respuesta
 					u.setNombreCompleto(json_data.getString("NOMBRE_COMPLETO_USUARIO"));
-					// u.setLatitud(Double.parseDouble(json_data.getString("Latitud")));
 					u.setEmail(json_data.getString("EMAIL"));
 					return true;
 				} catch (Exception e) {
@@ -44,6 +36,45 @@ public class UsuarioSQL {
 			}
 		} catch (Exception e) {
 			Log.e(TAG, "cargarUsuario, " + e.toString());
+		}
+		return false;
+	}
+	
+	public Boolean cargarUsuarioIniciar(Usuario u) {
+		try {
+			Parametros postParametersToSend = new Parametros();
+			postParametersToSend.add("nombre", u.getNombreUsuario());
+			JSONArray jdata = null;
+			jdata = post.getServerData(postParametersToSend, URL_connect + "getPersonaIniciar.php");
+			if (jdata != null) {
+				try {
+					JSONObject json_data = jdata.getJSONObject(0); // Se lee la respuesta
+					if(json_data.getString("NOMBRE_COMPLETO_USUARIO") != null);
+						return true;
+				} catch (Exception e) {
+					Log.e(TAG, "cargarUsuario, " + e.toString());
+				}
+			}
+		} catch (Exception e) {
+			Log.e(TAG, "cargarUsuarioIniciar, " + e.toString());
+		}
+		return false;
+	}
+
+	public Boolean persistir(Usuario u){
+		try {
+			Parametros postParametersToSend = new Parametros();
+			postParametersToSend.add("nombreUsuario", u.getNombreUsuario());
+			postParametersToSend.add("nombreCompleto", u.getNombreCompleto());
+			postParametersToSend.add("pass", u.getPassword());
+			postParametersToSend.add("email", u.getEmail());
+			JSONArray jdata = null;
+			jdata = post.getServerData(postParametersToSend, URL_connect + "ingresarUsuario.php");
+			if (jdata != null) {
+				return true;
+			}
+		} catch (Exception e) {
+			Log.e(TAG, "persistir, " + e.toString());
 		}
 		return false;
 	}
