2068814fbd792a5e187ca7a6b1487ce34ae7ad53
dcadiz.g@gmail.com <dcadiz.g@gmail.com@4bce4bf9-6550-0d38-5ae3-8cf01e3bfa03>
2012-12-19 00:46:59 +0000

    Mejorado la consulta al gps

--- trunk/AT/src/cl/at/bussines/GMapsAPI.java
+++ trunk/AT/src/cl/at/bussines/GMapsAPI.java
@@ -38,7 +38,6 @@ public class GMapsAPI {
 	public GMapsAPI(MapView m, Coordenada centro, float zoom){
 		this.mapView = m;
 		this.mapView.setBuiltInZoomControls(true);
-		//damian
 		Double latitud;
 		Double longitud;
 		this.locManager = (LocationManager)m.getContext().getSystemService(Context.LOCATION_SERVICE);
@@ -49,14 +48,32 @@ public class GMapsAPI {
 		}catch(Exception e){
 			Log.e(TAG, e.toString());
 			//TODO obtener ultima posicion desde la base de datos...
-			latitud = -20.213839 * 1E6; longitud = -70.152500 * 1E6;}
-		//fin damian
+			latitud = -20.213839 * 1E6; longitud = -70.152500 * 1E6;
+			}
 		this.centro = new GeoPoint(latitud.intValue(), longitud.intValue());
-//		this.centro = new GeoPoint((int)(centro.getLatitude()*1E6),(int) (centro.getLongitude()*1E6));
 		this.zoom = zoom;
 		this.mapController = mapView.getController();
 		mapController.setCenter(this.centro);
 		mapController.setZoom(this.zoom.intValue());
+		locListener = new LocationListener() {
+	    	public void onLocationChanged(Location location) {
+	    		mapView.getOverlays().clear();
+	    		Log.i(TAG, "loc.Lat: "+location.getLatitude()+" - loc.Long:"+location.getLongitude());
+	    		Coordenada c = new Coordenada(location.getLatitude(), location.getLongitude());
+	    		Log.i(TAG, "c.Lat: "+c.getLatitud()+" - c.Lon: "+c.getLongitud());
+	    		desplegarMapa(c);
+	    	}
+	    	public void onProviderDisabled(String provider){
+	    		Log.i(TAG, "GPS Status: OFF");
+	    	}
+	    	public void onProviderEnabled(String provider){
+	    		Log.i(TAG, "GPS Status: ON");
+	    	}
+	    	public void onStatusChanged(String provider, int status, Bundle extras){
+	    		Log.i(TAG, "Provider Status: " + status);
+	    	}
+    	};
+    	this.locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0, this.locListener);
 	}
 
 	public GMapsAPI(MapView m, Coordenada centro){
@@ -76,20 +93,9 @@ public class GMapsAPI {
 	}
 
 	public void setCentro(Coordenada centro) {
-//		if(this.location!=null){
-//			Log.i(TAG, "Actualizando location...");
-//			Double latitud = this.location.getLatitude()*1E6;
-//			Double longitud = this.location.getLongitude()*1E6;
-//			Log.i(TAG, "Lat Loc: "+latitud/1E6+" - Lon Loc: "+longitud/1E6);
-//			this.centro = new GeoPoint(latitud.intValue(), longitud.intValue());
-//			mapController.setCenter(this.centro);
-//		}
-//		else{
 		Log.i(TAG, "Lat Loc: "+centro.getLatitud()*1E6+" - Lon Loc: "+centro.getLongitud()*1E6);
-			this.centro = new GeoPoint((int)(centro.getLatitud()*1E6),(int) (centro.getLongitud()*1E6));
-			mapController.setCenter(this.centro);
-//		}
-
+		this.centro = new GeoPoint((int)(centro.getLatitud()*1E6),(int) (centro.getLongitud()*1E6));
+		mapController.setCenter(this.centro);
 	}
 	
 	public Coordenada getCentro() {
@@ -118,26 +124,6 @@ public class GMapsAPI {
 		OverlayItem overlayitem = new OverlayItem(centro, "Iquique","hola mundo!");
 		itemizedoverlay.addOverlay(overlayitem);
 		mapOverlays.add(itemizedoverlay);
-		locListener = new LocationListener() {
-	    	public void onLocationChanged(Location location) {
-	    		mapView.getOverlays().clear();
-	    		Log.i(TAG, "loc.Lat: "+location.getLatitude()+" - loc.Long:"+location.getLongitude());
-	    		Coordenada c = new Coordenada(location.getLatitude(), location.getLongitude());
-	    		Log.i(TAG, "c.Lat: "+c.getLatitud()+" - c.Lon: "+c.getLongitud());
-	    		desplegarMapa(c);
-	    	}
-	    	public void onProviderDisabled(String provider){
-	    		Log.i(TAG, "GPS Status: OFF");
-	    	}
-	    	public void onProviderEnabled(String provider){
-	    		Log.i(TAG, "GPS Status: ON");
-	    	}
-	    	public void onStatusChanged(String provider, int status, Bundle extras){
-	    		Log.i(TAG, "Provider Status: " + status);
-	    	}
-    	};
-    	this.locManager.requestLocationUpdates(
-    			LocationManager.GPS_PROVIDER, 20000, 0, this.locListener);
 	}
 
 	public void dibujarPoligono(ArrayList<Coordenada> dots){
