0940eee2ee2625a8a08499cc094edb9bf4aa387c
crgarrido.s@gmail.com <crgarrido.s@gmail.com@4bce4bf9-6550-0d38-5ae3-8cf01e3bfa03>
2012-12-19 11:13:33 +0000

    chichi!!!!!!!!!! cambios en los constructores

--- trunk/AT/src/cl/at/bussines/GrupoFamiliar.java
+++ trunk/AT/src/cl/at/bussines/GrupoFamiliar.java
@@ -2,35 +2,63 @@ package cl.at.bussines;
 
 import java.util.ArrayList;
 
+import cl.at.data.GrupoFamiliarSQL;
+import cl.at.data.LiderSQL;
+import cl.at.data.PuntoEncuentroSQL;
+import cl.at.data.UsuarioSQL;
+
 public class GrupoFamiliar {
 
+	private Integer id;
 	private String nombre;
 	private ArrayList<Comentario> comentarios;
 	private PuntoEncuentro puntoEncuentro;
 	private ArrayList<Usuario> integrantes;
 	private Lider lider;
-	
+
 	public GrupoFamiliar() {
 		comentarios = new ArrayList<Comentario>();
 		integrantes = new ArrayList<Usuario>();
 	}
 
 	public GrupoFamiliar(String nombre, PuntoEncuentro puntoEncuentro, Lider lider) {
-		lider.setGrupoFamiliar(this);	
+		lider.setGrupoFamiliar(this);
 		comentarios = new ArrayList<Comentario>();
 		integrantes = new ArrayList<Usuario>();
 		this.setNombre(nombre);
 		this.setPuntoEncuentro(puntoEncuentro);
 		this.setLider(lider);
-		
-	}	
+
+	}
 
 	// Setter y getter
-	
+
+	public GrupoFamiliar(Usuario usuario) {
+		GrupoFamiliarSQL gfSQL = new GrupoFamiliarSQL();
+		boolean flag = gfSQL.cargarGrupoFamiliar(this, usuario);
+		if (flag) {
+			PuntoEncuentroSQL ptoEncuentro = new PuntoEncuentroSQL();
+			puntoEncuentro = ptoEncuentro.cargarPtoEncuentro(this);
+			UsuarioSQL uSQL = new UsuarioSQL();
+			lider = usuario.esLider() ? new Lider(usuario) : uSQL.cargarLider(this);
+			integrantes = uSQL.cargarIntegrantes(this);
+			ComentarioSQL cSQL = new ComentarioSQL();
+			comentarios = cSQL.cargarComentarios(this);
+		}
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	
+
 	public String getNombre() {
 		return nombre;
 	}
@@ -38,15 +66,15 @@ public class GrupoFamiliar {
 	public void addComentario(Comentario comentario) {
 		this.comentarios.add(comentario);
 	}
-	
+
 	public ArrayList<Comentario> getComentarios() {
 		return this.comentarios;
 	}
-	
+
 	public void setPuntoEncuentro(PuntoEncuentro puntoEncuentro) {
 		this.puntoEncuentro = puntoEncuentro;
 	}
-	
+
 	public PuntoEncuentro getPuntoEncuentro() {
 		return puntoEncuentro;
 	}
@@ -63,15 +91,15 @@ public class GrupoFamiliar {
 	public void eliminarIntegrante(Usuario usuario) {
 		this.integrantes.remove(usuario);
 	}
-	
+
 	private void setLider(Lider lider) {
 		this.lider = lider;
 	}
-	
+
 	public Lider getLider() {
 		return lider;
 	}
-	
+
 	public void finalize() throws Throwable {
 
 	}
