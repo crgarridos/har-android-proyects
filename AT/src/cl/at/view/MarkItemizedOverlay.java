ccd2e55f3c0cd9dd791793e770c64583df411d10
dcadiz.g <dcadiz.g@gmail.com>
2012-12-28 03:30:23 +0000

    Corregido mal uso de los Overlay Se cambio icono de punto de encuentro se agregaron ADD especificos para cada tipo de punto

--- AT/src/cl/at/view/MarkItemizedOverlay.java
+++ AT/src/cl/at/view/MarkItemizedOverlay.java
@@ -6,10 +6,15 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
 
+import cl.at.bussines.Coordenada;
+import cl.at.bussines.PuntoEncuentro;
+import cl.at.bussines.Usuario;
+
+import com.google.android.maps.GeoPoint;
 import com.google.android.maps.ItemizedOverlay;
 import com.google.android.maps.OverlayItem;
 
-public class MarkItemizedOverlay extends ItemizedOverlay{
+public class MarkItemizedOverlay extends ItemizedOverlay<OverlayItem>{
 
 	private ArrayList<OverlayItem> mOverlays = new ArrayList<OverlayItem>();
 	private Context mContext;
@@ -22,6 +27,24 @@ public class MarkItemizedOverlay extends ItemizedOverlay{
 		  super(boundCenterBottom(defaultMarker));
 		  mContext = context;
 	}
+	
+	public void addPunto(Coordenada coordenada){
+		GeoPoint posicion = new GeoPoint((int)(coordenada.getLatitud()*1E6), (int)(coordenada.getLongitud()*1E6));
+		OverlayItem overlayItem = new OverlayItem(posicion, "Punto", "Posicion: "+coordenada.getLatitud()+" / "+coordenada.getLongitud());
+		addOverlay(overlayItem);
+	}
+	
+	public void addIntegrante(Usuario usuario){
+		GeoPoint posicion = new GeoPoint((int)(usuario.getDispositivo().getPosicion().getLatitud()*1E6), (int)(usuario.getDispositivo().getPosicion().getLongitud()*1E6));
+		OverlayItem overlayItem = new OverlayItem(posicion, usuario.getNombreUsuario(), "Posicion: "+usuario.getDispositivo().getPosicion().getLatitud()+" / "+usuario.getDispositivo().getPosicion().getLongitud());
+		addOverlay(overlayItem);
+	}
+	
+	public void addPuntoEncuentro(PuntoEncuentro puntoEncuentro){
+		GeoPoint posicion = new GeoPoint((int)(puntoEncuentro.getCoordenada().getLatitud()*1E6), (int)(puntoEncuentro.getCoordenada().getLongitud()*1E6));
+		OverlayItem overlayItem = new OverlayItem(posicion, "Punto de encuentro", puntoEncuentro.getReferencia());
+		addOverlay(overlayItem);
+	}
 
 	@Override
 	protected OverlayItem createItem(int i) {
@@ -30,7 +53,11 @@ public class MarkItemizedOverlay extends ItemizedOverlay{
 
 	public void addOverlay(OverlayItem overlay) {
 	    mOverlays.add(overlay);
-	    populate();
+//	    populate();
+	}
+	
+	public void grabar(){
+		populate();
 	}
 
 	@Override
