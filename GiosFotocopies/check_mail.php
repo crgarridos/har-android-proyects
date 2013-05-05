b2c7d89e3dc5a6854d29904cfc234d74f6c1011e
crgarrido.s <crgarrido.s@gmail.com>
2013-05-05 12:18:17 +0000

    MailManager ajustado a un singleton

--- GiosFotocopies/check_mail.php
+++ GiosFotocopies/check_mail.php
@@ -1,5 +1,5 @@
 <?php
 include './utils/class_MailManager.php';
-$json["cantidad"] = MailManager::getTotalUnread(MailManager::GMAIL_IMAP, "crgarrido.s@gmail.com", "password");
+$json["cantidad"] = MailManager::getTotalUnread(MailManager::GMAIL_IMAP, "misterharry.com@gmail.com", "0303456");
 echo json_encode($json);
 ?>
